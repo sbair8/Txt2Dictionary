39143072bd783f27ae88909f81e1200a512f5f00
sbair8 <sbair8@gmail.com>
2016-06-20 16:30:06 -0500

    Update AssemblyInfo.cs

--- Txt2Dir/Properties/AssemblyInfo.cs
+++ Txt2Dir/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TextFile to Dictionery")]
+[assembly: AssemblyTitle("TextFile to Dictionary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mach NL")]
